317dfc745040eac71c92af5560dc2ccc681162d5
simon0820s <simon0820s@gmail.com>
2023-05-23 13:14:58 -0500

    set winner & block game

--- src/App.jsx
+++ src/App.jsx
@@ -20,24 +20,56 @@ const Square = ({ children, updateBoard, index, isSelected }) => {
   );
 };
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 //App
 const App = () => {
   const [board, setBoard] = useState(Array(9).fill(null));
-
   const [turn, setTurn] = useState(TURNS.X);
+  const [winner, setWinner] = useState(null);
+
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo;
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] == boardToCheck[b] &&
+        boardToCheck[a] == boardToCheck[c]
+      ) {
+        return boardToCheck[a];
+      }
+    }
+
+    return null;
+  };
 
   const updateBoard = (index) => {
+    //no repeat validator
+    if (board[index] || winner) return;
 
-    if (board[index]) return
-
+    //update board
     const newBoard = [...board];
-    console.log(index)
     newBoard[index] = turn;
     setBoard(newBoard);
 
+    const newWinner = checkWinner(newBoard);
+
+    if (newWinner) {
+      setWinner(newWinner);
+    }
+
+    //toggle turn
     const newTurn = turn == TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
-
   };
 
   return (
